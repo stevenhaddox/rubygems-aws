380c8654ae9580929df84f3b66be2c6c85c452ce
Nathan Anderson <andnat@gmail.com>
2013-02-03 23:16:04 -0500

    All Vagrant boxes complete chef run, app server still can't run db:migrate for the vagrant env, it keeps running the production env.

--- chef/site-cookbooks/rubygems/recipes/database.rb
+++ chef/site-cookbooks/rubygems/recipes/database.rb
@@ -3,11 +3,42 @@
 # Recipe:: database
 #
 
-secrets = data_bag_item("secrets", "rubygems")
-rubygems_settings = secrets["application"][node["application"]["rails_env"]]
-rails_postgresql_user     = rubygems_settings["rails_postgresql_user"]
-rails_postgresql_password = rubygems_settings["rails_postgresql_password"]
-rails_postgresql_db = rubygems_settings["rails_postgresql_db"]
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+
+include_recipe "rubygems::default"
+
+app                       = node["application"]
+
+rails_env                 = node["application"]["rails_env"]
+rails_root                = app['rails_root']
+app_env                   = "#{app['name']}-#{rails_env}"
+Chef::Log.warn app_env.inspect
+sudo_name                 = app_env.tr("-", "_").upcase
+bundle_cmd                = "bundle"
+first_server_name         = app["server_names"][0]
+db_name                   = app_env.tr("-", "_")
+rails_postgresql_user     = app["name"]
+Chef::Log.warn rails_postgresql_user.inspect
+
+hba_cidr = if node.attribute?('ec2')
+             # ec2 private subnet
+             "10.0.0.0/8"
+           else
+             # assume vagrant?
+             "33.33.33.0/8"
+           end
+
+node.set['postgresql']['listen_addresses'] = node['ipaddress']
+node.set['postgresql']['pg_hba'] = [{
+  "type" => "host",
+  "db" => db_name,
+  "user" => rails_postgresql_user,
+  "addr" => hba_cidr,
+  "method" => "md5"
+}]
+
+# The password is stored in a data bag that is not in the repository.
+rails_postgresql_password = node.run_state[:app_secrets]["application"][rails_env]["rails_postgresql_password"]
 
 # create a DB user
 pg_user rails_postgresql_user do
@@ -16,7 +47,7 @@ pg_user rails_postgresql_user do
 end
 
 # create a database
-pg_database rails_postgresql_db do
+pg_database db_name do
   owner rails_postgresql_user
   encoding "utf8"
   template "template0"
--- chef/site-cookbooks/rubygems/recipes/default.rb
+++ chef/site-cookbooks/rubygems/recipes/default.rb
@@ -4,3 +4,8 @@
 #
 
 include_recipe "rubygems::users"
+
+app = node['application']
+app_secrets = data_bag_item("secrets", "rubygems")
+node.run_state[:app] = app.to_hash
+node.run_state[:app_secrets] = app_secrets.to_hash
